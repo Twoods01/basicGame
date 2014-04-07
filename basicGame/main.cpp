42eb39f0820874fc9a39682874812f9571a2a474
Twoods01 <twoods0129@gmail.com>
2014-04-07 13:31:00 -0700

    Initial Commit

---
+++ basicGame/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  basicGame
+//
+//  Created by Taylor Woods on 4/7/14.
+//  Copyright (c) 2014 Taylor Woods. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+   // insert code here...
+   std::cout << "Hello, World!\n";
+    return 0;
+}
+
